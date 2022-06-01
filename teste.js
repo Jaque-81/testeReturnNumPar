c3b0076e4ca4d1f44ad755f89f782d6ca9aabab3
Jaque Lima ✿ <jaqueline.lima.jpl@gmail.com>
2022-06-01 14:31:36 -0300

    first commit

---
+++ teste.js
@@ -0,0 +1,13 @@
+function returnNumsPar(array) {
+  let nums = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      nums.push(array[i])
+    }
+  }
+  console.log(nums)
+}
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+returnNumsPar(array);
